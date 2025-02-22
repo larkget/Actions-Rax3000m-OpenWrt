7d19dbe5043802e39f91057b8f9aff4ea1fc3d78
larkget <122@cosine90.top>
2025-02-22 02:13:45 +0100

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -24,7 +24,7 @@ rm -rf feeds/luci/applications/{luci-app-passwall,luci-app-ssr-plus,luci-app-pas
 #echo "src-git PSW https://github.com/sbwml/openwrt_helloworld.git;v5" >> "feeds.conf.default"
 #echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
 
-# echo 'src-git kwrt https://github.com/kiddin9/kwrt-packages' >> feeds.conf.default
+echo 'src-git kwrt https://github.com/kiddin9/kwrt-packages' >> feeds.conf.default
 
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
