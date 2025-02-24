3729aaed5860d79ea858c87a31ecf8e9f8f5815a
larkget <122@cosine90.top>
2025-02-24 15:37:43 +0100

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -27,9 +27,9 @@
 #echo 'src-git kwrt https://github.com/kiddin9/kwrt-packages' >> feeds.conf.default
 
 
-echo 'src-git shidahuilang https://github.com/shidahuilang/openwrt-package' >> feeds.conf.default
+#echo 'src-git shidahuilang https://github.com/shidahuilang/openwrt-package' >> feeds.conf.default
 
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >> feeds.conf.default
 
-#echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages' >>feeds.conf.default
+echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages' >> feeds.conf.default
 
